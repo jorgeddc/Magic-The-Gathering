e163f1aee7e72dff6bf1e3027061209b1916b6ac
Jorge Diaz <jorgeddc@gmail.com>
2021-12-02 22:08:37 +0100

    ya se ven las carticas

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const cards = []
+const root = document.querySelector('.root')
+const ul = document.createElement('ul')
 fetch ("https://api.magicthegathering.io/v1/cards")
 
 .then ((result) => result.json())
@@ -9,8 +11,18 @@ pintarCards()
 
 
 function pintarCards()
-  {
+  {   
     
-     console.log(cards[0])
+ cards[0].cards.forEach(element=>{
+  let li = document.createElement('li')
+  let imagen = document.createElement('img')
+  imagen.src = element.imageUrl
+  li.appendChild(imagen)
+  ul.appendChild(li)
+root.appendChild(ul)
+ 
+
+   // root.innerHTML += `<img src=${element.imageUrl}> </img>`
   
-  }
+  })
+}
