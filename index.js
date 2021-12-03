1bbd91eb3d1f9ad3efe3b89aeedce1d3e684810f
Jorge Diaz <jorgeddc@gmail.com>
2021-12-03 09:50:04 +0100

    element

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ fetch ("https://api.magicthegathering.io/v1/cards")
 .then ((result) => result.json())
 .then (data => {
 cards.push(data)
+
 pintarCards()
 })
 
@@ -14,12 +15,21 @@ function pintarCards()
   {   
     
  cards[0].cards.forEach(element=>{
+   console.log(element)
   let li = document.createElement('li')
   let imagen = document.createElement('img')
+  let p = document.createElement('p')
+  
+  let name = element.name
   imagen.src = element.imageUrl
-  li.appendChild(imagen)
-  ul.appendChild(li)
-root.appendChild(ul)
+ 
+
+    // p.appendChild(name)
+    li.appendChild(imagen)
+    p.appendChild(name)
+    li.appendChild(p)
+    ul.appendChild(li)
+    root.appendChild(ul)
  
 
    // root.innerHTML += `<img src=${element.imageUrl}> </img>`
