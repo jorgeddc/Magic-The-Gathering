f11d349caa7533bc43d52478b2c0c10fc0e9aff7
jorgeddc <85868623+jorgeddc@users.noreply.github.com>
2021-12-03 18:44:27 +0100

    pensando

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 const cards = []
-const root = document.querySelector('.root')
-const ul = document.createElement('ul')
+const root = document.querySelector(".root")
+const ul = document.createElement("ul")
 fetch ("https://api.magicthegathering.io/v1/cards")
 
 .then ((result) => result.json())
 .then (data => {
 cards.push(data)
-
 pintarCards()
 })
 
 
 function pintarCards()
-  {   
+  {
     
- cards[0].cards.forEach(element=>{
-   console.log(element)
-  let li = document.createElement('li')
-  let imagen = document.createElement('img')
-  let p = document.createElement('p')
-  
-  let name = element.name
-  imagen.src = element.imageUrl
- 
+     cards[0].cards.forEach(element=>{
+       const li = document.createElement("li")
+       const img = document.createElement("img")
+       const p = document.createElement("p")
+       img.src = element.imageUrl
+       let imagenNo = img.src
+       let name = element.name
+      console.log(imagenNo)
+    
+       p.innerText+=name
+       li.appendChild(p)
+       if (imagenNo!="http://127.0.0.1:5501/undefined")
+       {
+       li.appendChild(img)
+       ul.appendChild(li)
+       root.appendChild(ul)      
+      }
+      
+      
+      
+       
 
-    // p.appendChild(name)
-    li.appendChild(imagen)
-    p.appendChild(name)
-    li.appendChild(p)
-    ul.appendChild(li)
-    root.appendChild(ul)
- 
-
-   // root.innerHTML += `<img src=${element.imageUrl}> </img>`
+     })
   
-  })
-}
+  }
+
