5c7ccae2f083dfe38e0e7f372a99c48121a12fb9
Jorge Diaz <jorgeddc@gmail.com>
2021-12-12 14:00:22 +0100

    dichosso colores

--- draggable.js
+++ draggable.js
@@ -1,64 +1,74 @@
-function recojo (containerCard)
+
+let indexColor=0;
+function recojo (index)
 {
-    let cont = 0 ;
-    const draggable = document.querySelectorAll('.cards') 
-    const containerWhite = document.querySelectorAll('.item-container')
-    
-    draggable.forEach(element =>{
-        element.addEventListener('dragstart', ()=>{
-            element.classList.add('dragging')
-        })
-        element.addEventListener('dragend', ()=>{
-            element.classList.remove('dragging')
-        })
-    })
-    
-    
-    containerWhite.forEach(element=>{
-        element.addEventListener('dragover', e =>{
-            e.preventDefault()
-          
-         
-        })
-        element.addEventListener('drop', e =>{
-            e.preventDefault()
-            const encima = document.querySelector('.dragging')
-            //COLOCA LAS CARTAS EN ORDEN
-            if (cont===0){containerWhite[2].appendChild(encima)}
-            if (cont===1){ containerWhite[1].appendChild(encima)}
-            if (cont===2 ){ containerWhite[0].appendChild(encima)}
-            document.querySelector(".item-container").classList.add(`onew${cont}`)
-            cont++
-
-            if (cont>=0){ 
-             
-        draggable.forEach(element =>{ 
-            document.querySelector('.cards').classList.add('ajuste')
-            document.querySelector('.cartas').classList.add('entrar')
-            document.querySelector('p').classList.add('texto')})
-           
-        }
-          
-    
-            
-
-         
-            
-       containerWhite.forEach(element =>{  
-          
-           if (containerWhite[0].classList.contains("onew0") && containerWhite[0].classList.contains("onew1") && containerWhite[0].classList.contains("onew2"))
-            {
-               
-               document.querySelector(".color-mag-white").classList.add("active")
-            }
-        })
-         
-          
-            
-     
-           
    
-    
-     })
-})
+    let cont = 0 ;
+            const draggable = document.querySelectorAll('.cards') 
+            const containerWhite = document.querySelectorAll('.item-container')
+            
+            draggable.forEach(element =>{
+                element.addEventListener('dragstart', ()=>{
+                    element.classList.add('dragging')
+                    
+                })
+                element.addEventListener('dragend', ()=>{
+                    element.classList.remove('dragging')
+                    colorCorrecto()
+                    
+                })
+            })
+            
+            //   if (stringColors =='White')
+            //   {
+                
+                containerWhite.forEach(element=>{
+                element.addEventListener('dragover', e =>{
+                    e.preventDefault()
+                 
+                   
+                   
+                })
+                
+                
+                element.addEventListener('drop', e =>{
+                    e.preventDefault()
+                    const encima = document.querySelector('.dragging')
+                    //COLOCA LAS CARTAS EN ORDEN
+                    if (cont===0){containerWhite[2].appendChild(encima)}
+                    if (cont===1){ containerWhite[1].appendChild(encima)}
+                    if (cont===2 ){ containerWhite[0].appendChild(encima)}
+                    document.querySelector(".item-container").classList.add(`onew${cont}`)
+                    cont++
+                    
+                    if (cont>=0){ 
+                        draggable.forEach(element =>{ 
+                            document.querySelector('.cards').classList.add('ajuste')
+                            document.querySelector('.cartas').classList.add('entrar')
+                            document.querySelector('p').classList.add('texto')
+                    
+                            
+                        })
+                        
+                    }
+                    containerWhite.forEach(element =>{  
+                        
+                        if (containerWhite[0].classList.contains("onew0") && containerWhite[0].classList.contains("onew1") && containerWhite[0].classList.contains("onew2"))
+                        {
+                            
+                            document.querySelector(".color-mag-white").classList.add("active")
+                        }
+                    })
+                })  
+                
+            })
 }
+function colorCorrecto (color)
+{
+    console.log(color[indexColor])
+}
+//}
+
+
+
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cardsArray = []
-let CardsArray = []
+let ColoR  = []
+let j=0;
 const root = document.querySelector(".root")
 const ul = document.createElement("ul")
 
@@ -12,53 +13,69 @@ fetch ("https://api.magicthegathering.io/v1/cards")
 })
 
 
+
 function pintarCards()
 {   
   
-  cardsArray[0].cards.map((element ) =>{
-  let i= Math.floor(Math.random() * 100)
-
+  cardsArray[0].cards.forEach((element,index) =>{
+    let i= Math.floor(Math.random() * 100)
+ 
+    
     const li = document.createElement("li")
     const img = document.createElement("img")
     img.classList.add('cartas')
     const p = document.createElement("p")
     const containerCard = document.createElement('div')
     
-       img.src =  cardsArray[0].cards[i].imageUrl
-       let imagenNo = img.src
-       let name =cardsArray[0].cards[i].name
+    img.src =  cardsArray[0].cards[i].imageUrl
+    let imagenNo = img.src
+    let name =cardsArray[0].cards[i].name
+    let colors =  cardsArray[0].cards[i].colors 
+    let stringColors = String(colors) 
+    
+     if(j==0) //ESTA CONDICION ENVIA SOLO UN ARRAY RELLENO CON LOS COLORES
+     { 
+       ColoR.push(stringColors)
+      colorCorrecto(ColoR)
+    }  
+      j++
      
     
-       p.innerText+=name
-       li.appendChild(p)
-       if (imagenNo!="http://127.0.0.1:5501/undefined")
-       {
-       li.appendChild(img)
-       containerCard.appendChild(li)      
-       ul.appendChild(containerCard)
-       root.appendChild(ul)
-      }
-      
-      
-      mov(containerCard)
-      recojo(containerCard) 
-
-     })
-     
-  function mov(containerCard){
+        
     
+    
+    
+    p.innerText+=name
+    li.appendChild(p)
+    if (imagenNo!="http://127.0.0.1:5501/undefined")
+    {
+      li.appendChild(img)
+      containerCard.appendChild(li)      
+      ul.appendChild(containerCard)
+         root.appendChild(ul)
+        }
+        
+        
+        mov(containerCard)
+        recojo()
+        
+        
+      })
+      
+      function mov(containerCard){
+        
         containerCard.classList.add("cards")
         const card = document.querySelectorAll('.cards')
         card.forEach((element,index) =>{  
-        element.addEventListener('mouseover', e =>{
-      
-          card[index].classList.add('tePille')
-
-             VanillaTilt.init(document.querySelector('.tePille'), {
+          element.addEventListener('mouseover', e =>{
+            
+            card[index].classList.add('tePille')
+            
+            VanillaTilt.init(document.querySelector('.tePille'), {
               max: 25,
-             speed: 400
-         });
-        })
+              speed: 400
+            });
+          })
        element.addEventListener('mouseout', e =>{
          card[index].classList.remove('tePille')
        })
