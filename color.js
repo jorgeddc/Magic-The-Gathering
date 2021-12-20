c4504c75f57513219ce38d03fdeb79c2739794b6
Jorge Diaz <jorgeddc@gmail.com>
2021-12-20 23:35:12 +0100

    viendo la luz

--- color.js
+++ color.js
@@ -2,18 +2,24 @@ arrayColorW=[]
 arrayColorB=[]
 arrayColorBl=[]
 
-function chooseColor (encima,color){
-  console.log(encima)
+function chooseColor (encima,color,draggable){
+  console.log(draggable)
 switch (color) {
     case "White":
    arrayColorW.push(color)
   
      if (arrayColorW.length === 1) { 
-        document.querySelectorAll(`.item-${color}`)[2].appendChild(encima)}
+        document.querySelectorAll(`.item-${color}`)[2].appendChild(encima)
+      ajusteCards(color,draggable)}
         if (arrayColorW.length === 2) { 
-          document.querySelectorAll(`.item-${color}`)[1].appendChild(encima)}
+          document.querySelectorAll(`.item-${color}`)[1].appendChild(encima)
+      ajusteCards(color,draggable)}
+    
           if (arrayColorW.length === 3) { 
-            document.querySelectorAll(`.item-${color}`)[0].appendChild(encima)}
+            document.querySelectorAll(`.item-${color}`)[0].appendChild(encima)
+            document.querySelector('.color-mag-white').classList.add('active')
+      ajusteCards(color,draggable)}
+    
         
     
      
@@ -26,11 +32,20 @@ switch (color) {
       arrayColorB.push(color)
   
       if (arrayColorB.length === 1) { 
-         document.querySelectorAll(`.item-${color}`)[2].appendChild(encima)}
+         document.querySelectorAll(`.item-${color}`)[2].appendChild(encima)
+      ajusteCards(color,draggable)}
+        
+        
          if (arrayColorB.length === 2) { 
-           document.querySelectorAll(`.item-${color}`)[1].appendChild(encima)}
+           document.querySelectorAll(`.item-${color}`)[1].appendChild(encima)
+      ajusteCards(color,draggable)}
+    
            if (arrayColorB.length === 3) { 
-             document.querySelectorAll(`.item-${color}`)[0].appendChild(encima)}
+             document.querySelectorAll(`.item-${color}`)[0].appendChild(encima)
+            document.querySelector('.color-mag-blue').classList.add('active')
+
+      ajusteCards(color,draggable)}
+    
     
       break
 
@@ -38,12 +53,50 @@ switch (color) {
       arrayColorBl.push(color)
   
       if (arrayColorBl.length === 1) { 
-         document.querySelectorAll(`.item-${color}`)[2].appendChild(encima)}
+         document.querySelectorAll(`.item-${color}`)[2].appendChild(encima)
+      ajusteCards(color,draggable)}
+    
          if (arrayColorBl.length === 2) { 
-           document.querySelectorAll(`.item-${color}`)[1].appendChild(encima)}
+           document.querySelectorAll(`.item-${color}`)[1].appendChild(encima)
+      ajusteCards(color,draggable)
+    }
            if (arrayColorBl.length === 3) { 
-             document.querySelectorAll(`.item-${color}`)[0].appendChild(encima)}
+             document.querySelectorAll(`.item-${color}`)[0].appendChild(encima)
+            document.querySelector('.color-mag-black').classList.add('active')
+
+      ajusteCards(color,draggable)
+    }
       break;
    
   }
 }
+function ajusteCards(color,draggable)
+{
+  const texto= document.querySelectorAll('p')
+  const cartas = document.querySelectorAll('.cartas')
+   
+  draggable.forEach(element=>{
+    element.classList.add('ajuste')
+  })
+cartas.forEach(element=>{
+  element.classList.add('entrar')
+})
+ texto.forEach(element=>{
+   element.classList.add('texto')
+ })
+
+document.querySelector(`.item-${color}`).classList.add(`onew${contador}`)
+const containerWhite = document.querySelectorAll('.item-container')
+
+
+
+
+containerWhite.forEach(element =>{  
+
+if ( document.querySelectorAll(`.item-${color}`).classList.contains("onew0") &&  document.querySelectorAll(`.item-${color}`).classList.contains("onew1") &&  document.querySelectorAll(`.item-${color}`).classList.contains("onew2"))
+{
+
+document.querySelector(".color-mag-white").classList.add("active")
+}
+}) 
+} 
--- draggable.js
+++ draggable.js
@@ -3,7 +3,6 @@ function recojo (contador, imagenNo,color)
    
    
           
-             const containerWhite = document.querySelectorAll('.item-container')
              const draggable = document.querySelectorAll('.cards')
      
      
@@ -20,75 +19,50 @@ function recojo (contador, imagenNo,color)
                          
                          
                          
+                         
                         })
                     })
                     
                     draggable.forEach(element =>{    
                         element.addEventListener('dragend', ()=>{
                             
+                            
                             const encima = document.querySelector('.dragging')
-                            chooseColor(encima,color)
+                            chooseColor(encima,color,draggable)
                             element.classList.remove('dragging')
+                            
                             if (imagenNo=="http://127.0.0.1:5501/undefined"){
+                                
+                                
+                                
+                                
+                            }
+                            else{ pintarCards()
+                                
+                            }
+                            
+                            
+                            
+                        })
+                    })
                     
-                  
-                   
-                   
-                }
-                else{ pintarCards()
-                   }
-                
-                
-                
-            })
-        })
-            
-
-        
-    document.querySelectorAll(`.item-${color}`).forEach((element,index)=>{
-        element.addEventListener('dragover', e =>{
-            e.preventDefault()
-            
-        })
-        
-        element.addEventListener('drop', e =>{
-            e.preventDefault()
-            
-           // COLOCA LAS CARTAS EN POSICION QUE QUIERO
-            
-            //COLOCA LAS CART
-            
-        //     if (contador===0)
-        //     {  document.querySelectorAll(`.item-${color}`)[2].appendChild(encima)
-        //     }
-        //    if (contador===1){ 
-        //     document.querySelectorAll(`.item-${color}`)[1].appendChild(encima)
-        //      }
-        //      if (contador===2) { 
-        //         document.querySelectorAll(`.item-${color}`)[0].appendChild(encima)}
-         
-        
-        const cartasDentro = document.querySelector('.cards')
-          
-         cartasDentro.classList.add('ajuste')
-        document.querySelector('.cartas').classList.add('entrar')
-        document.querySelector('p').classList.add('texto')
-    document.querySelector(`.item-${color}`).classList.add(`onew${contador}`)
-
-    
-
-
-containerWhite.forEach(element =>{  
-
-    if ( document.querySelectorAll(`.item-${color}`).classList.contains("onew0") &&  document.querySelectorAll(`.item-${color}`).classList.contains("onew1") &&  document.querySelectorAll(`.item-${color}`).classList.contains("onew2"))
-    {
-    
-        document.querySelector(".color-mag-white").classList.add("active")
-    }
-}) }) })  
-}
-
-
+                    
+                    
+                    document.querySelectorAll(`.item-${color}`).forEach((element,index)=>{
+                        
+                        
+                        element.addEventListener('drop', e =>{
+                            e.preventDefault()
+                          
+                            
+                            
+                            
+                            
+                            
+                        }) })  
+                    }
+                    
+ 
 
 }
 
